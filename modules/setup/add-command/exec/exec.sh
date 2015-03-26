3fc7b72de053da40fea0cab3d344bafc065f88da
浦瀬信介 <urasin2012@gmail.com>
2015-03-26 16:47:52 +0900

    second commit

--- modules/setup/add-command/exec/exec.sh
+++ modules/setup/add-command/exec/exec.sh
@@ -3,7 +3,7 @@ read module
 echo "作成するコマンド名を入力して下さい"
 read com
 
-mkdir -p ${BASE_DIR}/modules/${module}/${com}
+mkdir -p ${BASE_DIR}/modules/${module}/${com}/exec
 touch ${BASE_DIR}/modules/${module}/explain.txt
 touch ${BASE_DIR}/modules/${module}/${com}/explain.txt
-touch ${BASE_DIR}/modules/${module}/${com}/exec.sh
+touch ${BASE_DIR}/modules/${module}/${com}/exec/exec.sh

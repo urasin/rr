ac1e2510e242f61a493ba307b3a51453bad7a5f6
浦瀬信介 <urasin2012@gmail.com>
2015-03-31 18:31:10 +0900

    error処理を追加

--- command_list.sh
+++ command_list.sh
@@ -4,6 +4,6 @@ ls -1 ${BASE_DIR}/modules/${MODULE}/ | grep -v explain.txt > ${BASE_DIR}/tmp2016
 echo "#### Command List: ${MODULE} #####"
 while read com
 do
-    echo "${MODULE}:$com \"`cat ${BASE_DIR}/modules/${MODULE}/${com}/explain.txt 2>> /dev/null`\""
+    echo "  ${MODULE}:$com \"`cat ${BASE_DIR}/modules/${MODULE}/${com}/explain.txt 2>> /dev/null`\""
 done < ${BASE_DIR}/tmp2016
 rm ${BASE_DIR}/tmp2016
